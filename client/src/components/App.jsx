3bd03f6f879e7a044e787dea8da71ad450d7f150
Louise Ly <ly.louise07@gmail.com>
2023-01-30 17:44:11 -0600

    Ready to push

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import StarRating from './reusableComponents/StarRating.jsx';
 
 const App = () => {
   return (
     <div>
       <h3>Project Atelier</h3>
-      <div>
-        <StarRating rating='2.5' className='my-star'/>
-      </div>
     </div>
   );
 };
