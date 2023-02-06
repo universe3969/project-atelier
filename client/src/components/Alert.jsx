00039f2210f0f6b6c7daead3a44cac093f6fea36
Louise Ly <ly.louise07@gmail.com>
2023-02-05 21:22:24 -0600

    added Alert

---
+++ client/src/components/Alert.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Alert.scss';
+
+const Alert = ({ type, message }) => {
+
+  let alertStyle;
+
+  switch (type) {
+  case 'success':
+    alertStyle = { backgroundColor: 'green' };
+    break;
+  case 'error':
+    alertStyle = { backgroundColor: 'red' };
+    break;
+  case 'warning':
+    alertStyle = { backgroundColor: 'yellow' };
+    break;
+  default:
+    break;
+  }
+
+  return (
+    <div
+      className='alert-container'
+      style={alertStyle}
+    >
+      {message}
+    </div>
+  );
+};
+
+export default Alert;
--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,17 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import NavBar from './NavBar.jsx';
+import Alert from './Alert.jsx';
 
 const App = () => {
+  const [productId, setProductId] = useState(3731);
   const [currentProduct, setCurrentProduct] = useState(null);
+  const [alertStatus, setAlertStatus] = useState(false);
 
+  let alertBar;
+  // let alertBar = <Alert type='error' message='Product not found'/>;
   useEffect(() => {
-    axios.get('http://localhost:3000/api/products/37311')
-      .then(({ data }) => setCurrentProduct(data));
-  }, []);
+    axios.get(`http://localhost:3000/api/products/${productId}`)
+      .then(({ data }) => setCurrentProduct(data))
+      .catch(err => {
+        alertBar = <Alert type='error' message='Product not found'/>;
+        setAlertStatus(true);
+      });
+  }, [productId]);
+
+  const handleSearch = (searchId) => {
+    if (!isNaN(+searchId)) {
+      setProductId(searchId);
+    } else {
+      console.log('invalid product ID');
+      alertBar = <Alert type='error' message='Invalid product ID'/>;
+      setAlertStatus(true);
+    }
+  };
 
   return (
     <div>
-      <h3>Project Atelier</h3>
+      <NavBar onSearch={handleSearch}/>
+      {alertStatus && alertBar}
     </div>
   );
 };
