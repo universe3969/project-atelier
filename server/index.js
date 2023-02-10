237ab373393ed8ce003fd146384f802d324d95e3
Louise Ly <ly.louise07@gmail.com>
2023-02-09 20:09:11 -0600

    Finished configuration

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 require('dotenv').config();
 const router = require('./routes');
 
@@ -8,11 +7,12 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
 app.use(router);
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
