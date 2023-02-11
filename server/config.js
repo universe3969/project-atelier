c3d25ca724c99a8a0ee7351695fcb1864a1198d6
Louise Ly <ly.louise07@gmail.com>
2023-02-11 12:32:21 -0600

    Added config.js to gitignore

--- server/config.js
+++ server/config.js
@@ -3,5 +3,5 @@ module.exports = {
   REVIEW_URL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews',
   QUESTIONS_URL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions',
   ANSWER_URL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/answers',
-  TOKEN: 'YOUR TOKEN'
+  TOKEN: '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'
 };
