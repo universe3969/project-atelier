18b69bcf6b70a0cba0ba2496534202865def71e2
Louise Ly <ly.louise07@gmail.com>
2023-02-11 14:28:39 -0600

    Change token

--- server/config.js
+++ server/config.js
@@ -3,5 +3,5 @@ module.exports = {
   REVIEW_URL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews',
   QUESTIONS_URL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions',
   ANSWER_URL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/answers',
-  TOKEN: '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'
+  TOKEN: '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'
 };
