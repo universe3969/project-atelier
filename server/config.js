308a95338aa53c092b4ac7954bdc139fefb4bc61
Louise Ly <112071316+inf0xy@users.noreply.github.com>
2023-02-11 20:56:37 -0600

    Delete config.js

--- server/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  PRODUCT_URL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products',
-  REVIEW_URL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews',
-  QUESTIONS_URL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/questions',
-  ANSWER_URL: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/qa/answers',
-  TOKEN: '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'
-};
